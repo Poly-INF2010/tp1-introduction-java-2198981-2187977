12dfb5597c08bfa484b96825c06904518f83ca9b
antoinefischer <antoine-anh-minh@polymtl.ca>
2024-02-05 17:54:14 -0500

    N fini

--- src/main/java/Letter/LetterFactory.java
+++ src/main/java/Letter/LetterFactory.java
@@ -65,7 +65,7 @@ public final class LetterFactory {
 
         Rectangle highBar = new Rectangle(stripeThickness, maxHeight);
         Rectangle highBar2 = highBar.clone();
-        highBar2.translate(highBar2.getCoords(), new Point2d(maxWidth, 0.0));
+        highBar2.translate(highBar2.getCoords(), new Point2d((maxWidth-stripeThickness), 0.0));
         Rectangle middleBar = new Rectangle(maxWidth, stripeThickness);
         middleBar.translate(middleBar.getCoords(), new Point2d(0.0, halfMaxHeight));
 
@@ -82,7 +82,19 @@ public final class LetterFactory {
      * @return BaseShape containing the letter N
      */
     public static BaseShape create_N() {
-        return null;
+
+        Rectangle highBar = new Rectangle(halfStripeThickness, maxHeight);
+        Rectangle highBar2 = highBar.clone();
+        highBar2.translate(highBar2.getCoords(), new Point2d((maxWidth-halfStripeThickness), 0.0));
+        Rectangle highBar3 = highBar2.clone();
+        highBar3.rotate(highBar3.getCoords(), 315.0);
+
+        BaseShape N = new BaseShape();
+        N.add(highBar);
+        N.add(highBar2);
+        N.add(highBar3);
+
+        return N;
     }
 
     /** TODO
